ca55c9937eacc8f54087e8cef42440f8590a2b8e
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-24 14:45:52 +0700

    answer: palindrome number leet code

---
+++ 2.js
@@ -0,0 +1,65 @@
+/**
+ * 9. Palindrome Number
+ * 
+ * Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false
+    }
+
+
+    let a = x
+    let reversed = 0
+    while (x > 0) {
+
+        b = x % 10
+        reversed = reversed * 10 + b
+         x = Math.floor(x / 10)
+    }
+        return a === reversed
+
+};
